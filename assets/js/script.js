4c4fb6e804ca8636f781aafa4bef821e57d13823
Weslley Teodoro dos Santos <94250413+WestSantos@users.noreply.github.com>
2022-03-05 23:22:03 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
-Apagar
+
+const listaOpcoes = document.querySelector(".secaoDetalhesConta_opcoesTransacoes")
+
+listaOpcoes.addEventListener("click", identificarOpcoes)
+
+function identificarOpcoes(event){
+
+    const elemento = event.target
+
+    if(elemento.tagName == "LI"){
+
+        const id = elemento.id
+
+        const secaoEscolha = document.querySelector(`div[data-id="${id}"]`)
+
+        removeClasseMostrar()
+       
+        secaoEscolha.classList.add("mostrar")
+    }
+}
+
+function removeClasseMostrar(){
+    const divs = document.querySelectorAll(".secaoTransacao .container div")
+    for(let i = 0; i < divs.length; i++){
+
+        divs[i].classList.remove("mostrar")
+    }
+}
